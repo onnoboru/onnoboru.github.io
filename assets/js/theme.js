f9316a2173c78d04ea51ab3302205782b882937e
onnoboru <onnoboru.ml@gmail.com>
2026-06-22 23:55:59 +0900

    change default mode

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -270,7 +270,8 @@ let transTheme = () => {
 let determineThemeSetting = () => {
   let themeSetting = localStorage.getItem("theme");
   if (themeSetting != "dark" && themeSetting != "light" && themeSetting != "system") {
-    themeSetting = "system";
+    // Default to light instead of system so the site shows light mode by default.
+    themeSetting = "light";
   }
   return themeSetting;
 };
