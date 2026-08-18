13be2a5a33d1e0eb8eedb63c770512dbbf584861
onnoboru <69897023+onnoboru@users.noreply.github.com>
2026-08-18 15:15:19 +0000

    Deploying to gh-pages from @ onnoboru/onnoboru.github.io@dcdc7e8df9a672591e68a07320db0b11e76bd7d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,12 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-launched-my-website",
+        },{id: "news-launched-my-website",
           title: 'launched my website',
           description: "",
           section: "News",handler: () => {
@@ -52,16 +47,6 @@ ninja.data = [{
           description: "I built SO-101 arm.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
         id: 'social-cv',
         title: 'CV',
